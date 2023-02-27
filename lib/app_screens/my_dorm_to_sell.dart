7bceb9a4c32ca8f7eca841fed1e06df58b4a3b96
Thanyalak Prampree <prampreethanyalak@kkumail.com>
2023-02-28 01:09:08 +0700

    Update my_dorm_to_sell.dart

---
+++ lib/app_screens/my_dorm_to_sell.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class MyDormToSell extends StatefulWidget {
+  const MyDormToSell({super.key});
+
+  @override
+  State<MyDormToSell> createState() => _MyDormToSellState();
+}
+
+class _MyDormToSellState extends State<MyDormToSell> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
