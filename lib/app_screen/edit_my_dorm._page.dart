93a66ffaf4c6ef9c19fea2f2b0fe0991685af8c5
Thanyalak Prampree <prampreethanyalak@kkumail.com>
2023-02-28 02:35:17 +0700

    ้้ฟ้ฟ้

---
+++ lib/app_screen/edit_my_dorm._page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class EditMyDormPage extends StatefulWidget {
+  const EditMyDormPage({super.key});
+
+  @override
+  State<EditMyDormPage> createState() => _EditMyDormPageState();
+}
+
+class _EditMyDormPageState extends State<EditMyDormPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/filter_scrollable.dart
+++ lib/filter_scrollable.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_application_1/material_color.dart';
+import 'package:side_sheet/side_sheet.dart';
 
 class FilterSelected extends StatefulWidget {
   const FilterSelected({super.key});
@@ -11,6 +12,7 @@ class FilterSelected extends StatefulWidget {
 }
 
 class _FilterSelectedState extends State<FilterSelected> {
+  bool _isSelected = false;
   int i = 3;
 
   void _decrementI() {
@@ -23,35 +25,110 @@ class _FilterSelectedState extends State<FilterSelected> {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(
-    primary: Colors.blue, // Button background color
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(18.0),
-    ),
-    elevation: 5, // Elevation of the button
-  ),
-          onPressed: () {},
-          child:  Row(
-              children: [
-                Container(
-                    // padding: EdgeInsets.only(bottom: 10, right: 5),
-                    child: Padding(
-                  padding: const EdgeInsets.only(bottom: 20, right: 5),
-                  child: Icon(
-                    Icons.format_list_bulleted_rounded,
-                    color: Colors.white,
+        Container(
+          height: 35,
+          // alignment: Alignment.center,
+          margin: EdgeInsets.only(right: 5, bottom: 5, left: 5),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(18)),
+            color: myMaterialColor,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 1,
+                blurRadius: 2,
+                offset: Offset(0, 3),
+              ),
+            ],
+          ),
+          child: GestureDetector(
+            onTap: () => SideSheet.right(
+                body: SafeArea(
+                    child: Scaffold(
+                  bottomNavigationBar: BottomNavBar(),
+                  body: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              child: Text(
+                                "Filters",
+                                style: TextStyle(
+                                    fontFamily: "Comfortaa",
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w900,
+                                    color: Colors.black),
+                              ),
+                            ),
+                            Icon(
+                              Icons.list,
+                              color: Colors.black,
+                            )
+                          ],
+                        ),
+                        Divider(),
+                        Facility(
+                          facilities: [
+                            "เครื่องทำน้ำอุ่น",
+                            "ลิฟต์",
+                            'แอร์',
+                            'ตู้เย็น',
+                            'ทีวี',
+                            "เครื่องทำน้ำอุ่น",
+                            "ลิฟต์",
+                            'แอร์',
+                            'ตู้เย็น',
+                            'ทีวี',
+                            "เครื่องทำน้ำอุ่น",
+                            "ลิฟต์",
+                            'แอร์',
+                            'ตู้เย็น',
+                            'ทีวี',
+                            "เครื่องทำน้ำอุ่น",
+                            "ลิฟต์",
+                            'แอร์',
+                            'ตู้เย็น',
+                            'ทีวี',
+                            "เครื่องทำน้ำอุ่น",
+                            "ลิฟต์",
+                            'แอร์',
+                            'ตู้เย็น',
+                            'ทีวี',
+                            "เครื่องทำน้ำอุ่น",
+                            "ลิฟต์",
+                            'แอร์',
+                            'ตู้เย็น',
+                            'ทีวี'
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 )),
-                const Text(
-                  "Filter",
-                  style: TextStyle(
-                      fontFamily: "Comfortaa",
-                      fontSize: 15,
-                      fontWeight: FontWeight.w800,
-                      color: Colors.white),
-                ),
-              ],
+                context: context),
+            child: Container(
+              padding: EdgeInsets.only(left: 10, right: 10),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.list_outlined,
+                    color: Colors.white,
+                    size: 20,
+                  ),
+                  Padding(padding: EdgeInsets.only(right: 5)),
+                  Text(
+                    "Filters",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontFamily: "Comfortaa",
+                        fontSize: 15,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.white),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
@@ -109,6 +186,161 @@ class _FilterSelectedState extends State<FilterSelected> {
           ),
         ),
       ],
+    );
+  }
+}
+
+class Chipschoice extends StatefulWidget {
+  const Chipschoice({Key? key}) : super(key: key);
+
+  @override
+  State<Chipschoice> createState() => _ChipschoiceState();
+}
+
+class _ChipschoiceState extends State<Chipschoice> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return Colors.blue;
+      }
+      return Colors.indigo;
+    }
+
+    return Checkbox(
+      checkColor: Colors.white,
+      fillColor: MaterialStateProperty.resolveWith(getColor),
+      value: isChecked,
+      onChanged: (bool? value) {
+        setState(() {
+          isChecked = value!;
+        });
+      },
+    );
+  }
+}
+
+class Facility extends StatefulWidget {
+  final List<String> facilities;
+
+  const Facility({Key? key, required this.facilities}) : super(key: key);
+
+  @override
+  State<Facility> createState() => _FacilityState();
+}
+
+class _FacilityState extends State<Facility> {
+  @override
+  Widget build(BuildContext context) {
+    int mid = (widget.facilities.length / 1).ceil();
+    List<String> firstHalf = widget.facilities.sublist(0, mid);
+    List<String> secondHalf = widget.facilities.sublist(mid);
+
+    return Container(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Column(
+              children: firstHalf
+                  .map((facility) => buildFacilityRow(facility))
+                  .toList(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildFacilityRow(String facility) {
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(right: 10, left: 10),
+          child: Chipschoice(),
+        ),
+        Text(
+          facility,
+          style: TextStyle(fontFamily: "Bai Jamjuree", color: Colors.black),
+        ),
+      ],
+    );
+  }
+}
+
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      child: Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        height: 70,
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  //unselect all item
+                },
+                child: Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.all(7),
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 2, color: Colors.black)),
+                  child: Text(
+                    "Clear All",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontFamily: "Comfortaa",
+                        fontSize: 18,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.black),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 40,
+              ),
+              GestureDetector(
+                onTap: () {
+                  //add item filter and back home page
+                  Navigator.pushNamed(context, '/');
+                },
+                child: Container(
+                  alignment: Alignment.centerRight,
+                  padding: EdgeInsets.all(7),
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 2,
+                        color: Colors.black,
+                      ),
+                      color: Colors.black),
+                  child: Text(
+                    "Done",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontFamily: "Comfortaa",
+                        fontSize: 18,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.white),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
